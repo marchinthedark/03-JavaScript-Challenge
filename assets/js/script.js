90341ae465ebbcbac7a47d6c799985f0de50942a
Qassim Darawsheh <darawshehqassim@gmail.com>
2023-03-18 17:52:17 -0400

    added started code

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+var psasswordLenght;
+var lettersLowerCase;["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
